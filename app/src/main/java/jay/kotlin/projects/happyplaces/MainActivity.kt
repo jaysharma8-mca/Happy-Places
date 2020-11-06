fcd850684fa996c4b79dee3287118621e5ef0887
jaysharma8-mca <jaysharma155.cmpica@gmail.com>
2020-11-06 16:49:51 +0530

    initial commit

--- app/src/main/java/jay/kotlin/projects/happyplaces/MainActivity.kt
+++ app/src/main/java/jay/kotlin/projects/happyplaces/MainActivity.kt
@@ -1,11 +1,18 @@
 package jay.kotlin.projects.happyplaces
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppCenter.start(application, "efb997d0-c5dd-4768-94f8-b9fa07e82125",
+                Analytics::class.java, Crashes::class.java)
     }
 }
